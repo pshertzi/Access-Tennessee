cd6ab836d5c1d24d2d18a40ee01abc80340197e5
Paul Shertzinger <pshertzi@gmail.com>
2022-03-23 12:17:17 -0400

    handlebars added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers'));
+app.use(require('./controllers'));
 
 app.listen(PORT, () => {
     console.log(`You are now connected to port ${PORT}!`);
