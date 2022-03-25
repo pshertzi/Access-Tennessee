e3d75dbdf52ebd69a8d04c35b992c98c4e7d1273
Paul Shertzinger <pshertzi@gmail.com>
2022-03-25 14:22:08 -0400

    signup error fixed

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ require('dotenv').config(); //added for nodemail
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // turn on routes
 app.use(routes);
 
-
-
+//handle bars
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -85,5 +87,5 @@ app.post('/send', (req, res) => {
   
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}!`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
