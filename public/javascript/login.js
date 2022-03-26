15060ca4f1d9f42fb7b9b2013931dfa5f86fc01b
Paul Shertzinger <pshertzi@gmail.com>
2022-03-25 23:36:20 -0400

    login function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,25 @@
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const first_name = document.querySelector('#firstname-signup').value.trim();
-  const last_name = document.querySelector('#lastname-signup').value.trim();
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  const description = document.querySelector('#description-signup').value.trim();
+async function loginFormHandler(event) {
+    event.preventDefault();
   
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        username,
-        email,
-        password,
-        description
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    console.log(response);
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
   }
-}
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
