942f193626339ae3e253a2a5ab68d7c8a818a17f
Paul Shertzinger <pshertzi@gmail.com>
2022-03-29 15:24:35 -0400

    comment out logo const to allow signup to run

--- public/javascript/signIn.js
+++ public/javascript/signIn.js
@@ -38,10 +38,10 @@ async function b_signupFormHandler(event) {
   const b_email = document.querySelector('#b-email-signup').value.trim();
   const b_password = document.querySelector('#b-password-signup').value.trim();
   const b_description = document.querySelector('#b-description-signup').value.trim();
-  const logo_url = document.querySelector('#logo_url').value
+  //const logo_url = document.querySelector('#logo_url').value.trim();
   
 
-  if (b_username && b_email && b_password && b_description && b_username && logo_url) {
+  if (b_username && b_email && b_password && b_description && b_username) {
     const response = await fetch('/api/business', {
       method: 'post',
       body: JSON.stringify({
@@ -50,7 +50,7 @@ async function b_signupFormHandler(event) {
         b_email,
         b_password,
         b_description,
-        logo_url
+        //logo_url
       }),
       headers: { 'Content-Type': 'application/json' }
     });
