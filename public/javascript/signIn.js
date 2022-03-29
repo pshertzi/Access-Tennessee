08327a1e7f2a682a410a9cf57614a02866950d5d
Ashley Hodge <ashhodge@comcast.net>
2022-03-29 15:27:20 -0400

    minor changes

--- public/javascript/signIn.js
+++ public/javascript/signIn.js
@@ -38,10 +38,10 @@ async function b_signupFormHandler(event) {
   const b_email = document.querySelector('#b-email-signup').value.trim();
   const b_password = document.querySelector('#b-password-signup').value.trim();
   const b_description = document.querySelector('#b-description-signup').value.trim();
-  const logo_url = document.querySelector('#logo_url').value
+  // const logo_url = document.querySelector('#logo_url').value
   
 
-  if (b_username && b_email && b_password && b_description && b_username && logo_url) {
+  if (b_username && b_email && b_password && b_description && b_username) {
     const response = await fetch('/api/business', {
       method: 'post',
       body: JSON.stringify({
@@ -49,8 +49,7 @@ async function b_signupFormHandler(event) {
         b_username,
         b_email,
         b_password,
-        b_description,
-        logo_url
+        b_description
       }),
       headers: { 'Content-Type': 'application/json' }
     });
