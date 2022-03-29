62e6b308342af5b69553a16d51b202db695a244e
Paul Shertzinger <pshertzi@gmail.com>
2022-03-28 21:33:24 -0400

    posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -53,6 +53,22 @@ router.get('/userpage', (req, res) => {
     });
 });
 
+router.get('/post/:id', (req, res) => {
+  const post = {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  };
+
+  res.render('post', { Suggestion });
+});
+
 router.get('/signup', (req, res) => {
   res.render('signup');
 });
